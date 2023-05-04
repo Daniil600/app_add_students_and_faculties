fbb06a2dd887dca623aaadd6c6a0cd9e097a4b10
Daniil600 <ter6062@ya.ru>
2023-05-04 17:30:23 +0300

    1.Создали файл для миграций.
    2.Добавили два индекса, используя миграции:
    1) Индекс для поиска по имени студента.
    2) Индекс для поиска по названию и цвету факультета.

---
+++ src/main/resources/liquibase/scripts/course-four-lesson-three.sql
@@ -0,0 +1,5 @@
+-- liquibase formatted sql
+-- changeset dsazonov:1
+    CREATE INDEX student_name_index ON student(name);
+    CREATE INDEX faculty_name_index ON faculty(name);
+    CREATE INDEX faculty_color_index ON faculty(color);
